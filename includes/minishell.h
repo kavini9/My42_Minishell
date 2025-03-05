97a03c9aec044ef8ff23d3bb5331422571d031b0
Weerasinghe Arachchige Kavini Wageesha Weerasinghe <wweerasi@c1r5p11.hive.fi>
2025-03-05 22:29:46 +0200

    main loop progressing

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,17 +6,29 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:42:33 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/03/03 19:24:26 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/03/05 22:21:43 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <readline/readline.h>
+# include <readline/history.h>
+
 # include "../lib/libft/libft.h"
 # include "../src/parse/parse.h"
 # include "../src/execute/execute.h"
 # include "../src/builtin/builtin.h"
 # include "../src/signal/signal.h"
+
+typedef struct s_msh
+{
+	char	*cwd;
+	char	*old_wd;
+	int	exit_code;
+	t_env	envl;
+	t_token	*token;
+}
 
 #endif
--- src/main/main.c
+++ src/main/main.c
@@ -6,15 +6,40 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:32:56 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/03/03 20:40:58 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/03/05 22:28:37 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../includes/minishell.h"
 
+char	*prompt_gen(t_msh *msh)
+{
+	if(ft_strcmp(msh -> cwd, msh -> old_wd)
+		       prompt_update(msh -> cwd);
+
+}
+
+void	minishell(t_msh *msh)
+{
+	char	*line;
+
+	while(1)
+	{
+		line = readline(prompt_gen(msh));
+	}
+}
+
+
 int	main(int ac, char **av, char **envp)
 {
-	if (ac > 1)
-
-	minishell_loop();
+	t_msh	msh
+	(void) av;
+	
+	if (ac != 1)
+		exit(error("# minishell: Error: Invalid number of arguments."
+		"\n# Usage: ./minishell", 2);
+	msh_init(&msh, envp);
+	minishell(&msh);
+	msh_clean($msh);
+	exit(msh.exitcode);
 }
