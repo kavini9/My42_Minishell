fda34ba91448de388e9bf5056ee0404950e89ba5
Oshintha Sandaruwan <155742370+Oshintha@users.noreply.github.com>
2025-03-14 19:06:08 +0200

    Update parse.h

--- src/parse/parse.h
+++ src/parse/parse.h
@@ -1,8 +1,28 @@
-//add header here
-
 #ifndef PARSE_H
-# define PARSE_H
+#define PARSE_H
 
-# include "../../includes/minishell.h"
+#include <stdbool.h>
+
+typedef struct s_cmd
+{
+    char            **args;
+    int             cmd_index;
+    struct s_cmd   *next;
+} t_cmd;
+
+typedef struct s_shell
+{
+    t_cmd   *cmds;
+    int     exit_stat;
+} t_shell;
+
+// Function Prototypes
+int     parse_and_validate_input(char **input, t_shell *mini);
+int     validate_input_syntax(char **input, t_shell *mini);
+int     parse_input(t_shell *mini);
+int     parse_cmd_string(t_shell *mini, t_cmd *cmd);
+int     cmd_string_while(t_shell *mini, t_cmd *cmd, int i, int *cmd_found);
+void    add_cmd(t_shell *mini, char **args);
+t_cmd   *create_cmd(char **args);
 
 #endif
