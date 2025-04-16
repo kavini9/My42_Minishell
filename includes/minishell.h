66f2e6a1499a4716811e6ef5d8d47ca7b47bae1f
Amarathun Hettiarachchi Wickramage Oshintha Sandaruwan <aoshinth@c1r5p1.hive.fi>
2025-04-16 17:35:38 +0300

    change cmd linked list to struct array

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: aoshinth <aoshinth@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:42:33 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/04/08 15:18:35 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/04/15 18:13:30 by aoshinth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,20 @@
 # define  ERROR_MSG "minishell: Error" // represents a enum containing all the err codes for now.
 # define  HEREDOC_MAX 16
 
+#include <signal.h>
 # include <readline/readline.h> //for readline
 # include <readline/history.h> //for readline add_history function
 # include <stdio.h> //temporary printf in msh_clean. delete later if you use your own
 # include <stdlib.h> //for exit
 # include <string.h> //for strerror(errnum)
 # include <errno.h> //for using errno in strerror
-//# include <unistd.h>//for fork
+# include <unistd.h>//for fork
 # include <sys/types.h>//for pid_t
 # include <fcntl.h>//for file open flags
+# include <stdbool.h>
+# include <stddef.h>
+
+extern volatile __sig_atomic_t g_sig;
 
 typedef enum e_do_err {
     LOG   = 0b001,
@@ -37,6 +42,7 @@ typedef enum e_err_type{
 	ERR_MALLOC,
 } t_err_type;
 
+
 /* typedef struct s_msh
 {
 	char	*cwd;
@@ -47,18 +53,20 @@ typedef enum e_err_type{
 	int		cmd_count; //  for command.c
 	int		hdocfd_l[HEREDOC_MAX];
 	int		exit_code;
-} t_msh;
+} t_msh; */
 
-# include "../lib/libft/libft.h
-# include "envp.h"
+
+# include "../lib/libft/libft.h"
+# include "signal_utils.h"
 # include "parse.h"
+# include "envp.h"
 # include "execute.h"
 # include "builtin.h"
-# include "signal.h"
 
-void msh_init(t_msh *msh, char **envp);
+
+/* void msh_init(t_msh *msh, char **envp);
 void	msh_loop(t_msh *msh);
 //void	msh_clean(t_msh *msh);
-int err_out(char *msg);
+int err_out(char *msg); */
 
 #endif
