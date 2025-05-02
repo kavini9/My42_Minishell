7ceb623a8d0983b0e0cea33687b43cb9a9e5ea69
kavini9 <kavini.w.weerasinghe@gmail.com>
2025-05-02 20:59:30 +0300

    parse aux structs initiated

--- src/parse_new/parse.c
+++ src/parse_new/parse.c
@@ -6,11 +6,39 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 22:29:12 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/05/01 16:45:28 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/05/02 20:56:12 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+void line_split_bypipe(t_msh *msh, char *line, char **seg_arr)
+{
+    char *start;
+    char *seg;
+    char *temp;
+    
+    start = line;
+    while (*line)
+    {
+        if (*line == '|' && !check_quotes(start, line))//change the check quotes function
+        {
+            seg = ft_substr(start, 0, line - start + 1);
+            temp = seg;
+            if (temp)
+            {
+                seg = ft_strtrim(temp, " \t\n\r\v");
+                free(temp);
+            }
+            if (!seg)
+                exit(printf("# minishell: Error:Malloc Fail.\n"));//TODO: ERROR MALLOC.
+            *seg_arr = seg;
+            seg_arr++;
+            start = line + 1;
+        }
+        line++;
+    }
+}
 
 int	count_pipes(char *line)
 {
@@ -27,23 +55,36 @@ int	count_pipes(char *line)
 	}
 	return (pipe_count);
 }
+void	init_token(t_msh *msh, int cmd_count)
+{
+	msh -> aux -> token = ft_calloc(cmd_count + 1, sizeof(t_token *));
+	if (!msh -> aux -> token)
+		exit(printf("# minishell: Error:Malloc Fail.\n"));//TODO: ERROR MALLOC. Free aux -> seg.
+	while (cmd_count--)
+	{
+		*(msh -> aux -> token) = ft_calloc(1, sizeof(t_token));
+		if (!*(msh -> aux -> token))
+			exit(printf("# minishell: Error:Malloc Fail.\n"));//TODO: ERROR MALLOC. Since we calloced we can free until we find it NULL.
+	}
+}
 
-int init_parse_structs(t_msh *msh, char *line)
+void init_parse_structs(t_msh *msh, char *line)
 {
 	msh -> cmd_count = count_pipes(line) + 1;
-	
-    
+	msh -> aux -> seg = ft_calloc(msh -> cmd_count + 1, sizeof(char *));
+	if (!msh -> aux -> seg)
+		exit(printf("# minishell: Error:Malloc Fail.\n"));//TODO: ERROR MALLOC
+	init_token(msh, msh -> cmd_count);
 }
 
 int	msh_parse(char *line, t_msh *msh)
 {
-	if (init_parse_structs(msh, line))
-		return (1);
-	if (line_split_bypipe(msh, line, msh -> aux -> seg))
-		return (1);
-	if (parse_line(msh))
-		return (1);
-	return (0);
+	t_parse	aux;//in static memory remember to assign to cmd struct before leaving this function.
+
+	msh -> aux = &aux;
+	init_parse_structs(msh, line);
+	line_split_bypipe(msh, line, msh -> aux -> seg);
+	seg_tokenize(msh);
 }
 
 int main(int ac, char **av, char **envp)
