87187caac50eff5ef07dd6bc226d8176ae8eec20
Kavini Weerasinghe <kavini.w.weerasinghe@gmail.com>
2025-05-11 18:26:55 +0300

    tokenize saved

--- src/parse_new/tokenize.c
+++ src/parse_new/tokenize.c
@@ -12,27 +12,59 @@
 
 #include "../../includes/minishell.h"
 
-void set_qoute_flag(t_parse *aux, char seg)
+int set_redir_type(t_token *token, char *seg)
 {
-    if ()
+    if (*seg == '<' && *(seg + 1) == '<')
+        token -> redir = REDIR_HDOC;
+    else if (*seg == '>' && *(seg + 1) == '>')
+        token -> redir = REDIR_APPEN;
+    else if (*seg == '<')
+        token -> redir = REDIR_INP;
+    else if (*seg == '>')
+        token -> redir = REDIR_OUTP;
+    return (1);
+}
+
+int get_token_len(t_token *token, char *seg, int tok_len)
+{
+    int redir_flg;
+    int quote_flg;
+    int is_white;
+
+    redir_flg = 0;
+    quote_flg = 0;
+    while (*seg)
+    {
+        is_white = ft_strchr(" \t\n\r\f\v", *seg);
+        if ((*seg == '\'' || *seg == '"') && !quote_flg)
+            quote_flg = (int) *seg;
+        else if ((*seg == '\'' || *seg == '"') && quote_flg == (int) *seg)
+            quote_flg = 0;
+        if (redir_flg && !is_white)
+            redir_flg = 0;
+        if ((*seg == '<' || *seg == '>') && !quote_flg && !redir_flg && tok_len)
+            break;
+        if ((*seg == '<' || *seg == '>') && !quote_flg && !redir_flg)
+            redir_flg = set_redir_type(token, seg);
+        if (is_white && !quote_flg && !redir_flg)
+            break;
+        tok_len++;
+        seg++;
+    }
+    return (tok_len);
 }
 
 char *extract_token(t_msh *msh, t_parse *aux, char *seg)
 {
-    t_token *token;
-    char    *tok_st;
-    int     tok_len;    
+    t_token ***token;
+    int     tok_len;
     
     token = aux -> token;
-    tok_st = seg;
-    tok_len = 0;
     while (*seg)
     {
-        if (*seg == '\'' || *seg == '"')
-            set_qoute_flag(aux, *seg);
-        if (*seg == '<' || *seg == '>' && !token -> quote)
-            set_redir_flag(aux, seg);
-        if ()
+        tok_len = get_token_len(token, seg, 0);
+        
+
     }
 }
 
@@ -45,7 +77,7 @@ void	seg_tokenize(t_msh *msh, t_parse *aux)
     while (*seg)
     {
         extract_token(msh, aux, *seg);
-        skip_whites(seg);
+
     } 
     
 }
