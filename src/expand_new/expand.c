ebb290f1464e61f0e7d3612447cd6430b56e9445
kavini9 <kavini.w.weerasinghe@gmail.com>
2025-05-16 21:52:03 +0300

    expansion in progress

--- src/expand_new/expand.c
+++ src/expand_new/expand.c
@@ -6,7 +6,7 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 22:11:45 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/05/05 22:12:25 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/05/16 21:50:59 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 void expand_and_setup_cmd(t_msh *msh, t_token **token)
 {
-
+    
 }
 
 
+// if the expansion is within the "" the field splitting is not applied to the expanded output. otherwise split it with the spaces.
 
+//walk through the string while removing quotes
+//save what was the last quote type
+//when found $ sign pass it to find key 
+//when the key is found pass it to expand with quoted or unquoted flag.
+//create a array with the
