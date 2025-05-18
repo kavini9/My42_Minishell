52e68b15bb57eec8173152401d84de322f1fc3a8
kavini9 <kavini.w.weerasinghe@gmail.com>
2025-05-18 23:54:49 +0300

    expansion started

--- src/expand_new/expand.c
+++ src/expand_new/expand.c
@@ -6,15 +6,39 @@
 /*   By: wweerasi <wweerasi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 22:11:45 by wweerasi          #+#    #+#             */
-/*   Updated: 2025/05/16 21:50:59 by wweerasi         ###   ########.fr       */
+/*   Updated: 2025/05/18 22:38:04 by wweerasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../includes/minishell.h"
 
-void expand_and_setup_cmd(t_msh *msh, t_token **token)
+void init_cmd_struct(t_msh *msh, int cmd_count)
 {
+    t_cmd **tmp_cmd;
     
+    msh -> cmd = ft_calloc(cmd_count + 1, sizeof(t_cmd *));
+    if (!msh -> cmd)
+		exit(printf("# minishell: Error:Malloc Fail.\n"));//TODO: ERROR MALLOC
+    tmp_cmd = msh -> cmd;
+    while (cmd_count--)
+    {
+        *tmp_cmd = ft_calloc(1, sizeof(t_cmd));
+        if (!*tmp_cmd)
+            exit(printf("# minishell: Error:Malloc Fail.\n"));//TODO: ERROR MALLOC
+        tmp_cmd++;
+    }
+}
+
+void expand_and_setup_cmd(t_msh *msh, t_token **token, t_cmd **cmd)
+{
+    while (*token)
+    {
+        while ((**token).token)
+        {
+
+               
+        }  
+    }
 }
 
 
@@ -25,3 +49,4 @@ void expand_and_setup_cmd(t_msh *msh, t_token **token)
 //when found $ sign pass it to find key 
 //when the key is found pass it to expand with quoted or unquoted flag.
 //create a array with the
+//tilde expansion should be done after variable expansion because variables can include tilde.
--- test_parse.c
+++ test_parse.c
@@ -245,6 +245,7 @@ void	msh_parse(char *line, t_msh *msh)
     print_segments(msh -> aux -> seg);
 	seg_tokenize(msh, msh -> aux);
     print_tokens(msh -> aux -> token);
+    init_cmd_struct(msh, msh -> cmd_count)
     //expand_and_setup_cmd(msh, msh -> aux -> token); 
 }
 
